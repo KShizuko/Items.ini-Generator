943018d99e88273c6cd0e958a65b43fa0ddfb39d
Kiyono <>
2023-09-14 21:27:56 +0200

    Restored from back-up

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+
+namespace ItemsINI_Batcher
+{
+    internal class Program
+    {
+        private static string GetQualityType(string ID)
+        {
+            switch ((uint.Parse(ID) % 10))
+            {
+                case 3:
+                    return "NORMALV1";
+
+                case 4:
+                    return "NORMALV2";
+
+                case 5:
+                    return "NORMALV3";
+
+                case 6:
+                    return "REFINED";
+
+                case 7:
+                    return "UNIQUE";
+
+                case 8:
+                    return "ELITE";
+
+                case 9:
+                    return "SUPER";
+            }
+            return "FIXED";
+        }
+
+        private static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine("The output will be located at;\r\n" + currentDirectory + @"\itemtype.txt");
+            Console.WriteLine("Press enter when you are ready to begin.");
+            Console.ReadLine();
+            if (File.Exists(currentDirectory + @"\itemtype.txt"))
+            {
+                TextReader TR = new StreamReader(currentDirectory + @"\itemtype.txt");
+                string Items = TR.ReadToEnd();
+                TR.Close();
+                Items = Items.Replace("\r", "");
+                string[] AllItems = Items.Split('\n');
+
+                string ItemName;
+                string Quality;
+                string ID;
+                int count = 0;
+                StreamWriter Writer = new StreamWriter(new FileStream(currentDirectory + @"\Items.ini", FileMode.Create));
+                Writer.WriteLine("[Items]");
+
+                foreach (string _item in AllItems)
+                {
+                    string _item_ = _item.Trim();
+                    if (_item_.Length >= 2)
+                    {
+                        if (_item_.IndexOf("//", 0, 2) != 0)
+                        {
+                            string[] data = _item_.Split(' ');
+                            if (data.Length >= 37)
+                            {
+                                ID = data[0].Trim();
+                                ItemName = data[1].Trim();
+                                Quality = GetQualityType(ID);
+                                Writer.WriteLine(ItemName.ToUpper() + Quality + "=" + ID);
+                            }
+                            count++;
+                            Console.WriteLine("Written: {0} items to Items.ini", count);
+                        }
+                    }
+                }
+                Writer.Flush();
+                Writer.Close();
+                Console.WriteLine("Finished, press enter to exit.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("itemtype.txt not found");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// Assembly Item INI Database Batcher, Version 1.0.0.0
+
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9  2008")]
+[assembly: System.Reflection.AssemblyTitle("Item INI Database Batcher")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Reflection.AssemblyProduct("Item INI Database Batcher")]
+[assembly: System.Runtime.InteropServices.Guid("75917c06-b1f3-4f23-b122-5bdce6e8cfe6")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+
